6f230b54bee278456f5cf371313c6c66798440e5
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-11-04 18:04:12 -0400

    update

--- myapp/app/fooditem/[id]/page.js
+++ myapp/app/fooditem/[id]/page.js
@@ -4,9 +4,12 @@ import React, { useEffect, useState } from 'react';
 const FoodItem = (ctx) => {
   const [foodDetails, setFoodDetails] = useState(null);
   const [prepStyle, setPrepStyle] = useState('');
-  const [quantity, setQuantity] = useState();
-  const [extras, setExtras] = useState([]);
+  const [price, setPrice] = useState(0);
+  const [quantity, setQuantity] = useState(1);
   const [size, setSize] = useState(0);
+  const [prep, setPrep] = useState([]);
+  const [extras, setExtras] = useState([]);
+
 
 
   useEffect(() => {
@@ -17,6 +20,7 @@ const FoodItem = (ctx) => {
         if (response.ok) {
           const food = await response.json();
           setFoodDetails(food);
+          setPrice(food.prices[0]);
         } else {
           console.error(`Error fetching food: ${response.status}`);
         }
@@ -28,15 +32,22 @@ const FoodItem = (ctx) => {
     fetchFood();
   }, []);
 
-  
+  const changePrice = (number) => {
+    setPrice(price + number);
+  };
 
-  const handleQuantityChange = (increment) => {
-    if (increment) {
-      setQuantity(quantity + 1);
-    } else if (quantity > 1) {
-      setQuantity(quantity - 1);
+  const handleChange = (e, option) => {
+    const checked = e.target.checked;
+
+    if (checked) {
+      changePrice(option.price);
+      setExtras((prev) => [...prev, option]);
+    } else {
+      changePrice(-option.price);
+      setExtras(extras.filter((extra) => extra._id !== option._id));
     }
   };
+
 
   return (
     <section className="text-gray-600 body-font overflow-hidden">
@@ -49,7 +60,7 @@ const FoodItem = (ctx) => {
           />
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
             <h2 className="text-sm title-font text-gray-500 tracking-widest">Seafood Delight</h2>
-            <span className="title-font font-medium text-2xl text-gray-900">${foodDetails?.prices[0]}</span>
+            <span className="title-font font-medium text-2xl text-gray-900">${price}</span>
             <div className="flex items-center space-x-4 mt-4">
               <p className="leading-relaxed lg:w-3/4">{foodDetails?.desc}</p>
             </div>
@@ -59,35 +70,45 @@ const FoodItem = (ctx) => {
                   <div className="mb-4">
                     <h3 className="text-gray-800 text-lg font-semibold">Select Meal Size</h3>
                     <div className="flex items-center space-x-2">
-    
+
                     </div>
                   </div>
                   <div>
                     <h3 className="text-gray-800 text-lg font-semibold">Preparation Style</h3>
-                    <div className="flex items-center space-x-4 mt-2">
-             
-                    </div>
+                    {foodDetails.prepstyle.map((option) => (
+                  <div key={option._id}>
+                    <label htmlFor={option.text}>
+                      <input
+                        type="checkbox"
+                        id={option.text}
+                        name={option.text}
+                        onChange={(e) => handleChange(e, option)}
+                        />
+                      {option.text}
+                    </label>
+                  </div>
+                ))}
                   </div>
                   <div className="mt-4">
                     <h3 className="text-gray-800 text-lg font-semibold">Quantity</h3>
                     <div className="flex items-center space-x-4">
                       <button
                         onClick={() => handleQuantityChange(false)}
-                        className="bg-indigo-500 text-white rounded-full w-8 h-8 flex items-center justify-center"
+                        className="bg-gray-500 text-white w-8 h-8 flex items-center justify-center"
                       >
                         -
                       </button>
                       <span>{quantity}</span>
                       <button
                         onClick={() => handleQuantityChange(true)}
-                        className="bg-indigo-500 text-white rounded-full w-8 h-8 flex items-center justify-center"
+                        className="bg-gray-500 text-white w-8 h-8 flex items-center justify-center"
                       >
                         +
                       </button>
                     </div>
                   </div>
                   <div className="mt-4">
-                    <button className="bg-indigo-500 text-white rounded-md px-4 py-2 hover:bg-indigo-700">
+                    <button className="bg-gray-500 text-white rounded-md px-4 py-2 hover:bg-indigo-700">
                       Add to Cart
                     </button>
                   </div>
